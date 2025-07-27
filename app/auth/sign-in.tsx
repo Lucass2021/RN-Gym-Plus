0fbd660520cb7cb71ffee7ee8ef47b1f62463733
Lucas Dias <lucas.allx@hotmail.com>
2025-07-27 11:35:27 -0300

    feat: translate sign-in screen initial

--- app/auth/sign-in.tsx
+++ app/auth/sign-in.tsx
@@ -1,10 +1,18 @@
-import TextComponent from "@/components/TextComponent";
-import {KeyboardAvoidingView, Platform, ScrollView, View} from "react-native";
-import {z} from "zod";
-import {FormProvider, useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
-import Input from "@/components/Input";
 import Button from "@/components/Button";
+import Input from "@/components/Input";
+import TextComponent from "@/components/TextComponent";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {router} from "expo-router";
+import {FormProvider, useForm} from "react-hook-form";
+import {useTranslation} from "react-i18next";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  ScrollView,
+  View,
+} from "react-native";
+import {z} from "zod";
 
 const signInFormSchema = z.object({
   email: z.email("Email inválido"),
@@ -14,6 +22,8 @@ const signInFormSchema = z.object({
 type SignInForm = z.infer<typeof signInFormSchema>;
 
 export default function SignIn() {
+  const {t} = useTranslation();
+
   const form = useForm<SignInForm>({
     resolver: zodResolver(signInFormSchema),
     defaultValues: {
@@ -30,53 +40,91 @@ export default function SignIn() {
     // router.replace("");
   };
 
+  const handleForgotPassword = () => {
+    console.log("handleForgotPassword");
+  };
+
   return (
     <KeyboardAvoidingView
       className="flex-1"
       behavior={Platform.OS === "ios" ? "padding" : "height"}>
-      <View className="flex-1 bg-white px-7.5 pt-20">
+      <View className="flex-1  bg-white px-7.5 pt-40 pb-14">
         <ScrollView
           keyboardShouldPersistTaps="handled"
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{
             flexGrow: 1,
             alignItems: "center",
+            justifyContent: "space-between",
           }}>
-          <TextComponent
-            color="primary"
-            fontFamily="BebasNeue"
-            fontWeight="Regular"
-            fontSize="h3"
-            customClassName="text-center">
-            Sign in to continue
-          </TextComponent>
-
-          <FormProvider {...form}>
-            <Input
-              name="email"
-              customInputTitle="E-mail"
-              customPlaceholder="Digite seu e-mail"
-              keyboardType="email-address"
-              autoCapitalize="none"
-              autoComplete="off"
-              autoCorrect={false}
-            />
-            <Input
-              name="password"
-              customInputTitle="Senha"
-              customPlaceholder="Digite sua senha"
-              keyboardType="default"
-              secureTextEntry
-            />
-          </FormProvider>
-
           <View className="w-full">
-            <Button
-              text="Entrar"
-              onPress={() => handleSubmit(handleSignIn)()}
-              disabled={false}
-            />
+            <TextComponent
+              color="primaryDark"
+              fontFamily="BebasNeue"
+              fontWeight="Regular"
+              fontSize="h3"
+              customClassName="text-center">
+              {t("auth.title")}
+            </TextComponent>
+
+            <FormProvider {...form}>
+              <Input
+                name="email"
+                customInputTitle={t("auth.emailCustomInputTitle")}
+                customPlaceholder={t("auth.emailCustomPlaceholder")}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoComplete="off"
+                autoCorrect={false}
+              />
+              <Input
+                name="password"
+                customInputTitle={t("auth.passwordCustomInputTitle")}
+                customPlaceholder={t("auth.passwordCustomPlaceholder")}
+                keyboardType="default"
+                secureTextEntry
+              />
+
+              <Pressable
+                onPress={() => handleForgotPassword()}
+                className="self-end">
+                <TextComponent
+                  color="dark"
+                  fontFamily="Inter"
+                  fontWeight="SemiBold"
+                  fontSize="paragraphOne">
+                  {t("auth.forgorPassword")}
+                </TextComponent>
+              </Pressable>
+            </FormProvider>
+
+            <View className="w-full mt-5">
+              <Button
+                text={t("auth.submitButton")}
+                onPress={() => handleSubmit(handleSignIn)()}
+                disabled={false}
+              />
+            </View>
           </View>
+
+          <Pressable onPress={() => router.push("/auth/sign-up")}>
+            <TextComponent
+              color="dark"
+              fontFamily="Inter"
+              fontWeight="Regular"
+              fontSize="subtitleOne"
+              customClassName="text-center">
+              {t("auth.sign-up-text-one")}{" "}
+              <TextComponent
+                color="primaryDark"
+                fontFamily="Inter"
+                fontWeight="Regular"
+                fontSize="subtitleOne"
+                customClassName="underline">
+                {t("auth.sign-up-text-two")}
+              </TextComponent>
+            </TextComponent>
+          </Pressable>
         </ScrollView>
       </View>
     </KeyboardAvoidingView>
